61951848f3dd104168a8e5ec35b80b50abd87b7b
Jiří Vrba <jiri@vrba.dev>
2021-01-26 12:55:49 +0100

    Added mod that turns eggs into implosion grenades

--- src/main/java/cz/vse/fis/minecraft/funshit/FunShit.java
+++ src/main/java/cz/vse/fis/minecraft/funshit/FunShit.java
@@ -6,9 +6,20 @@ public final class FunShit extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        registerEventListeners();
     }
 
     @Override
     public void onDisable() {
+    }
+
+    /**
+     * Register all plugin event listeners under this plugin
+     * Potentially this method can be later an IOC dependency injection entrypoint?
+     */
+    private void registerEventListeners() {
+        getServer()
+                .getPluginManager()
+                .registerEvents(new EggImplosionGrenadesListener(this), this);
     }
 }
